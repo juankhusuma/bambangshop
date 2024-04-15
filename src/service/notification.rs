b6d1ba709876430067b6b91b8e0f0c3f665b9700
juankhusuma <66712975+juankhusuma@users.noreply.github.com>
2024-04-15 16:07:46 +0700

    Added Reflection 3 on README.md

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -1,6 +1,6 @@
+use crate::model::notification::Notification;
 use crate::model::product::Product;
 use crate::model::subscriber::Subscriber;
-use crate::model::{notification::Notification, subscriber};
 use crate::repository::subscriber::SubscriberRepository;
 use bambangshop::{compose_error_response, Result};
 use rocket::http::Status;
